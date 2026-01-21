2d6c2993fef4fc3cf13aa9cca2005ba1acf4d268
probablyanocelot <58635071+probablyanocelot@users.noreply.github.com>
2026-01-21 15:56:40 -0600

    Refactor proxied_require to improve error handling and logging; replace character name references with a safe retrieval function

--- proxied_require.js
+++ proxied_require.js
@@ -2,84 +2,106 @@
 const proxied_require = (() => {
   const NODE_BASE = ".";
   const WEB_BASE =
-    "https://github.com/probablyanocelot/AdventureLandBots/tree/rework/";
+    "https://raw.githubusercontent.com/probablyanocelot/AdventureLandBots/rework/";
   const FOLDER = "/codes/";
   const tick_delay = () =>
     new Promise((r) => {
       setTimeout(r, 0);
     });
   const AsyncFunction = (() => async function () {})().constructor;
-  const COMPLETED_PROMISE = Promise.resolve(null);
   let module_cache = {};
+
+  function getCharacterName() {
+    try {
+      return typeof character !== "undefined" && character && character.name
+        ? character.name
+        : "unknown";
+    } catch (e) {
+      return "unknown";
+    }
+  }
+
+  function safeGameLog(msg) {
+    try {
+      if (typeof game_log === "function") {
+        game_log(msg);
+      } else {
+        log(msg);
+      }
+    } catch (e) {
+      log(msg);
+    }
+  }
+
   async function fetch(path_name, ret, name, handler) {
     try {
       let lib_name = name.split(".")[0];
       await tick_delay();
-      if (module_cache[name]) {
-        let result = await Promise.race([
-          module_cache[name],
-          COMPLETED_PROMISE,
-        ]);
-        if (result == null) {
-          game_log(
-            `(${character.name})[${path_name}]: Module ${name} found in cache, but not completed. Waiting for finish.`,
-          );
-          ret[lib_name] = (await module_cache[name]).exports;
-          game_log(
-            `(${character.name})[${path_name}]: Module ${name} finished loading.`,
-          );
-        } else {
-          game_log(
-            `(${character.name})[${path_name}]: Module ${name} found in cache.`,
-          );
-          ret[lib_name] = (await module_cache[name]).exports;
-        }
 
+      if (module_cache[name]) {
+        safeGameLog(
+          `(${getCharacterName()})[${path_name}]: Module ${name} found in cache.`,
+        );
+        ret[lib_name] = (await module_cache[name]).exports;
         return;
       }
+
       let resolve;
       module_cache[name] = new Promise((r) => (resolve = r));
-      game_log(`(${character.name})[${path_name}]: Fetching ${name}`);
+      safeGameLog(`(${getCharacterName()})[${path_name}]: Fetching ${name}`);
       let data = await handler(FOLDER + name);
-      game_log(`(${character.name})[${path_name}]: Fetched ${name}`);
+      safeGameLog(`(${getCharacterName()})[${path_name}]: Fetched ${name}`);
       await tick_delay();
+
       let func = AsyncFunction("module", "exports", "require", data + "");
       let _module = { exports: {} };
-      game_log(`(${character.name})[${path_name}]: Executing ${name}`);
+      safeGameLog(`(${getCharacterName()})[${path_name}]: Executing ${name}`);
       await func(
         _module,
         _module.exports,
         proxied_require.bind({ name: path_name + ":" + name }),
       );
-      game_log(`(${character.name})[${path_name}]: Executed ${name}`);
+      safeGameLog(`(${getCharacterName()})[${path_name}]: Executed ${name}`);
       resolve(_module);
       ret[lib_name] = _module.exports;
     } catch (e) {
-      console.log(
-        "(" + character.name + ")[" + path_name + "]: ERROR ENCOUNTERED: " + e,
-      );
-      console.log(
-        "(" +
-          character.name +
-          ")[" +
-          path_name +
-          "]: Offending script: " +
-          name,
-      );
+      try {
+        log(
+          "(" +
+            getCharacterName() +
+            ")[" +
+            path_name +
+            "]: ERROR ENCOUNTERED: " +
+            e,
+        );
+        log(
+          "(" +
+            getCharacterName() +
+            ")[" +
+            path_name +
+            "]: Offending script: " +
+            name,
+        );
+      } catch (_) {}
       throw e;
     }
-    return;
   }
+
   async function proxied_require(...libraries) {
-    const path_name = this?.name ?? character.name + ".js";
+    const path_name = this?.name ?? getCharacterName() + ".js";
     await tick_delay();
-    if (typeof parent.module != "undefined") {
+
+    const isNode =
+      typeof process !== "undefined" &&
+      process.versions &&
+      !!process.versions.node;
+
+    if (isNode) {
       let ret = {};
       let libs = libraries.map(async (name) => {
         await fetch(path_name, ret, name, async (file_name) => {
-          return await require("fs").promises.readFile("." + file_name);
+          return await require("fs").promises.readFile("." + file_name, "utf8");
         });
-        return;
       });
       await Promise.all(libs);
       return ret;
@@ -105,8 +127,23 @@ const proxied_require = (() => {
   return proxied_require;
 })();
 
-exports.proxied_require = proxied_require;
+// exports.proxied_require = proxied_require;
 
-const { moveInCircle } = await proxied_require("move_in_circle.js");
+// Replace the direct (non-async) require/use with an async IIFE that awaits the proxied_require result
+(async () => {
+  // Load the module (await the proxied_require call)
+  const libs = await proxied_require("move_in_circle.js");
+  // Module base name is the filename without extension: "move_in_circle"
+  const { moveInCircle } = libs.move_in_circle || {};
+  if (typeof moveInCircle !== "function") {
+    throw new Error("moveInCircle not found in move_in_circle.js exports");
+  }
 
-await moveInCircle({ x: 100, y: 100 }, 50, Math.PI);
+  async function doMove() {
+    await moveInCircle({ x: 100, y: 100 }, 50, Math.PI);
+  }
+
+  await doMove();
+})().catch((e) => {
+  log("Error loading/calling move_in_circle:", e);
+});
