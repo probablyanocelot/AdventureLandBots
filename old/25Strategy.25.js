d5a4b773216d4fdbb5e7577300c901f39bc4c885
probablyanocelot <58635071+probablyanocelot@users.noreply.github.com>
2026-01-28 11:48:55 -0600

    start of implementation - proxied_require

--- old/25Strategy.25.js
+++
@@ -1,4 +0,0 @@
-idleFarmWhitelist = [
-    'tortoise', 'squig', 'squigtoad', 'poisio', 'spider', 'bigbird',
-    'bbpompom', 'crabx', 'goo', 'cgoo', 'boar', 'bat', 
-];
--- old/merchant.52.js
+++
@@ -1,9 +0,0 @@
-import { Character } from "app.51.js";
-
-class Merchant extends Character {
-  constructor(data) {
-    super(data);
-  }
-
-  replaceTools() {}
-}
