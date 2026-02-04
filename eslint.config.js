ec8613e624f7f6617f5670c7a588611c6dfa9639
probablyanocelot <58635071+probablyanocelot@users.noreply.github.com>
2026-02-03 21:04:31 -0600

    feat: refactor ESLint configuration and implement delayed death handler in listeners

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+const globals = require("globals");
+
+module.exports = [
+  {
+    ignores: ["game_codes/**", "examples/**"],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
+];
